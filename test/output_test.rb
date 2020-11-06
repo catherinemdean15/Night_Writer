b7261a730436df5bd71061524961ceb5b007e484
Catherine Dean <catherinemdean15@gmail.com>
2020-11-06 12:48:55 -0700

    initial commit

---
+++ test/output_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/output'
+
+class OutputTest < Minitest::Test
+
+  def setup
+    @output = Output.new('braille.txt', 256)
+  end
+
+  def test_it_exitst
+    assert_instance_of Output, @output
+  end
+
+  def test_it_replies
+    expected = "Created 'braille.txt' containing 256 characters"
+    assert_equal expected , @output.reply
+  end
+
+end
