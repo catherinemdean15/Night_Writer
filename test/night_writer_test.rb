bce42eebacad64908d5fee8db5120b18bff83592
Catherine Dean <catherinemdean15@gmail.com>
2020-11-06 12:57:09 -0700

    create NightWriter class and test

---
+++ test/night_writer_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/output'
+require './lib/night_writer'
+
+class NightWriterTest < Minitest::Test
+
+  def setup
+    @night_writer = NightWriter.new
+  end
+
+  def test_it_exists
+    assert_instance_of NightWriter, @night_writer
+  end
+
+end
