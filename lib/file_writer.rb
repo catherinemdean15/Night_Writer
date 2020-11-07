ad777bf674a4d15383327ea02bb46aad72838613
Catherine Dean <catherinemdean15@gmail.com>
2020-11-07 10:36:29 -0700

    add read_file and write_file methods to FileWriter class

--- lib/file_writer.rb
+++ lib/file_writer.rb
@@ -2,8 +2,17 @@ class FileWriter
 
   def initialize(file_path)
     @file_path = file_path
+    @contents = nil
   end
 
+  def read_file
+    File.read(@file_path)
+  end
 
+  def write_file
+    new_file = File.new("braille.txt", "w")
+    new_file.write(read_file)
+    new_file.close
+  end
 
 end
--- test/file_writer_test.rb
+++ test/file_writer_test.rb
@@ -5,12 +5,22 @@ require './lib/file_writer'
 class FileWriterTest < Minitest::Test
 
   def setup
-    file_path = './message.txt'
+    file_path = 'message.txt'
     @file_writer = FileWriter.new(file_path)
   end
 
   def test_it_exists
     assert_instance_of FileWriter, @file_writer
+  end
+
+  def test_it_reads_file
+    assert_equal "hello world \n", @file_writer.read_file
+  end
+
+  def test_it_writes_new_file
+    new_file = 'braille.txt'
+    assert new_file, @file_writer.write_file
+    assert_equal "hello world \n", File.read(new_file)
   end
 
 end
