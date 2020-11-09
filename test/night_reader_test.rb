e04de2d8e150bb9dd757c67bea7478b7a3e1a44a
Catherine Dean <catherinemdean15@gmail.com>
2020-11-09 12:41:15 -0700

    refactor night_reader and night_writer

--- test/night_reader_test.rb
+++ test/night_reader_test.rb
@@ -1,6 +1,11 @@
-# require './test/test_helper'
-require 'minitest/autorun'
-require 'minitest/pride'
-require './lib/translation'
-require './lib/braille_translator'
-require './lib/format'
+require './test/test_helper'
+
+def test_it_writes_file
+  new_file = 'braille.txt'
+  assert new_file, @file_writer.write_file
+end
+
+def test_it_creates_output
+  @file_writer.stubs(:count_characters).returns(5)
+  assert_equal "Created 'sample.txt' containing 5 characters", @file_writer.create_output
+end
