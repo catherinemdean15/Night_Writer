961c84dc314aba04cbd78dc8ea70311c3d7c4513
Catherine Dean <catherinemdean15@gmail.com>
2020-11-08 17:13:41 -0700

    add row_to_letter method in Braille Translator

--- lib/braille_translator.rb
+++ lib/braille_translator.rb
@@ -2,5 +2,22 @@ class BrailleTranslator
 
   def initialize(contents)
     @contents = contents
+  end
+
+  def convert_to_rows
+    row_and_contents = Hash.new
+    @contents.split("\n").each_with_index do |chars, index|
+      row_and_contents[index] = chars
+    end
+    row_and_contents
+  end
+
+  def row_to_letter
+    row_and_letters = Hash.new
+    convert_to_rows.each do |row, contents|
+      row_and_letters[row] = contents.scan(/.{2}/)
+    end
+    row_and_letters
+  end
 
 end
--- test/braille_translator_test.rb
+++ test/braille_translator_test.rb
@@ -1,7 +1,7 @@
 # require './test/test_helper'
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/braille'
+require './lib/braille_translator'
 
 class BrailleTranslatorTest < Minitest::Test
 
@@ -12,6 +12,16 @@ class BrailleTranslatorTest < Minitest::Test
 
   def test_it_exists
     assert_instance_of BrailleTranslator, @braille_translator
+  end
+
+  def test_it_converts_to_rows
+    expected = {0 => "0.00", 1 => "0..0", 2 => "..00"}
+    assert_equal expected, @braille_translator.convert_to_rows
+  end
+
+  def test_it_splits_row_to_letter
+    expected = {0 => ["0.","00"], 1 => ["0.",".0"], 2 => ["..","00"]}
+    assert_equal expected, @braille_translator.row_to_letter
   end
 
 
