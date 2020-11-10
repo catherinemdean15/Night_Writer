e078123189f8d248ac0c05e25a3caeeba2c076ca
Catherine Dean <catherinemdean15@gmail.com>
2020-11-09 21:59:31 -0700

    refactor english_dictionary in Translation

--- test/translation_test.rb
+++ test/translation_test.rb
@@ -20,25 +20,15 @@ class TranslationTest < Minitest::Test
   def test_it_translates_one_character
     contents = "a"
     translation_3 = Translation.new(contents)
-    assert_equal [["0.","..",".."]], translation_3.translate
-  end
-
-  def test_braille_complete_letters
-    contents = "0.00\n0..0\n..00"
-    translation_3 = Translation.new(contents)
-    expected = [["0.","0.",".."], ["00",".0","00"]]
-    assert_equal expected, translation_3.braille_complete_letters
+    assert_equal [["0.","..",".."]], translation_3.translate(contents)
   end
 
   def test_it_translates_when_braille
-    contents = "0.00\n0..0\n..00"
+    contents = ["0.","..",".."]
     translation_3 = Translation.new(contents)
-    assert_equal "by", translation_3.translate
+    translation_3.stubs(:braille?).returns(true)
+    assert_equal "a", translation_3.translate(contents)
   end
 
-  def test_it_fomats_translation_to_braille
-    contents = [["0.", "00", ".."], ["0.", ".0", ".."], ["0.", "0.", "0."], ["0.", "0.", "0."], ["0.", ".0", "0."]]
-    assert_equal "0.0.0.0.0.\n00.00.0..0\n....0.0.0.\n", @translation.format_translation(contents)
-  end
 
 end
